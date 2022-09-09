3accbd328348caf4be29b8b7db252035feae4abc
samrashed <sambdtexas@yahoo.com>
2022-09-08 19:57:36 -0500

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'weather'`, () => {
+  it(`should have as title 'weatherAPI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('weather');
+    expect(app.title).toEqual('weatherAPI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('weather app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('weatherAPI app is running!');
   });
 });
